b87e2a3359f8f9f8f19246419612353baaac8ab4
zabiullah-shokuri <zabitawana4@gmail.com>
2024-10-23 15:07:04 +0330

    Add Header section

---
+++ src/app/components/MenuItem.jsx
@@ -0,0 +1,16 @@
+
+import Link from "next/link";
+
+
+function MenuItem({ title , address , Icon}) {
+  return (
+
+    <Link href={address} className="hover:text-amber-500">
+      <Icon className="text-2xl sm:hidden" />
+      <p className="uppercase hidden sm:inline text-sm">{title}</p>
+    </Link>
+  );
+}
+
+export default MenuItem;
+
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Header from "./components/Header";
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,10 +26,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+
+    
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+
+        <Header />
+          
+     
         {children}
       </body>
     </html>
